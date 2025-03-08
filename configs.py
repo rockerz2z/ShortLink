d3f3c4daeaa296447c6ff665aff515abf3dce2fb
Techify Bots <TechifyBots@gmail.com>
2025-03-08 23:56:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,8 +7,10 @@ BASE_URL = environ.get("BASE_URL", "")
 DATABASE_URL = environ.get("DATABASE_URL", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 ADMINS = int(environ.get("ADMINS", ""))
-AUTH_CHANNELS = environ.get("AUTH_CHANNEL", "")
+IS_FSUB = bool(environ.get("FSUB", False)) # Set "True" For Force Subscribe Enable
+AUTH_CHANNELS = environ.get("AUTH_CHANNEL", "") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
+
 
 START_TXT = '''<b>{},
 
@@ -31,6 +33,10 @@ Ex: <code>/shortlink example.com api</code>
 Now send me any link I will convet that link into your connected Shortener
 
 If you want to remove your Shortener then use <code>/remove</code> command.'''
+
+FORCE_SUBSCRIBE_TEXT = '''<b>{}, Tᴏ ᴜsᴇ ᴛʜᴇ ʙᴏᴛ ʏᴏᴜ ᴍᴜsᴛ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ғɪʀsᴛ. Tʜᴇ ʙᴏᴛ ᴡɪʟʟ ɴᴏᴛ ᴘʀᴏᴄᴇss ᴀɴʏ ʀᴇǫᴜᴇsᴛs ᴡɪᴛʜᴏᴜᴛ ᴊᴏɪɴɪɴɢ.
+
+बॉट का उपयोग करने के लिए आपको पहले हमारे चैनल में Join होना होगा। बॉट बिना शामिल हुए किसी भी Request को Process नहीं करेगा।</b>'''
 
 ABOUT_TXT = '''<b>╔════❰ ShortLink Bot ❱═══❍
 ║ ┏━━━━━━━━━❥
