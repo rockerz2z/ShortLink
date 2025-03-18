550efc0380c52b978459790685cd23283990ecdf
Techify Bots <TechifyBots@gmail.com>
2025-03-18 21:38:28 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import logging.config
-import sys
-
-logging.config.fileConfig('logging.conf')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
