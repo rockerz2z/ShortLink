36b03649fa0d75ba03601ba5ca87a009ed3e429c
Techify Bots <TechifyBots@gmail.com>
2025-06-10 21:18:41 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,43 @@
+class text(object):
+  START = """<b>{},
+
+I can convert your links into short links.
+
+Click the Help button below to get more info.
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+
+  LOG = """#New_User
+
+User: {}
+ID: {}"""
+  
+  ABOUT = """<b>📜 Cʜᴇᴄᴋ Aʙᴏᴜᴛ:
+  
+Lɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ 📚
+Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 🐍
+Sᴇʀᴠᴇʀ: Rᴇɴᴅᴇʀ 🌐
+Bᴜɪʟᴅ Sᴛᴀᴛᴜs: V1.3 🚀
+Sᴏᴜʀᴄᴇ Cᴏᴅᴇ: Aᴠᴀɪʟᴀʙʟᴇ (Fʀᴇᴇ) 💻 
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+  
+  HELP = """<b>🚀 Shortlink Bot Guide</b>
+
+Easily shorten any link using commands:
+
+<b>Examples:</b>
+◉ `/tiny https://youtube.com/@techifybots`
+◉ `/isgd https://youtube.com/@techifybots`
+◉ `/vgd https://youtube.com/@techifybots`
+
+<b>🔧 Set Your Preferred Shortener:</b>  
+◉ `/shortlink [site] [api_key]`
+
+<b>📋 Check Current Setup:</b>  
+◉ /info
+
+Once set, all links you send will be auto-shortened with your chosen service.  
+You can update your shortener anytime using the same command.
+
+<b><blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
